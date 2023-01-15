5c1deddbd15682950e373de51290bf75954816ec
PPFilip <7268500+PPFilip@users.noreply.github.com>
2023-01-15 01:11:58 +0100

    refactor for better readability

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use log::*;
 
 mod modbus_datatypes;
-use modbus_datatypes::*;
+use modbus_datatypes::ModbusConversions;
 
 /// Structure to store counter data in types compatible with psql
 struct Counter {
@@ -28,153 +28,42 @@ struct Measurement {
     i1_thd: f32,
     c1: Counter,
     c4: Counter,
-    x3: Counter
+    x3: Counter,
 }
 
 /// Connect to modbus server and get all measurements
-async fn get_measurements(modbus_conn_string: String, device_id: u8) -> Result<Measurement, Box<dyn std::error::Error>> {
+async fn get_measurements(
+    modbus_conn_string: String,
+    device_id: u8,
+) -> Result<Measurement, Box<dyn std::error::Error>> {
     use tokio_modbus::prelude::*;
 
     let socket_addr = modbus_conn_string.parse().unwrap();
     let mut ctx = tcp::connect(socket_addr).await?;
     ctx.set_slave(Slave(device_id));
 
-    let raw_runtime = ctx.read_input_registers(103, 2).await?;
-    let m_runtime = get_t3(raw_runtime.clone());
-    debug!("Run time is '{:?}' => '{:?}'", raw_runtime, m_runtime);
+    let m_runtime = read_finder_register!(ctx, "Run time", 103, 2, get_t3);
+    let m_freq = read_finder_register!(ctx, "Frequency", 105, 2, get_t5);
+    let m_u1 = read_finder_register!(ctx, "U1", 107, 2, get_t5);
+    let m_i1 = read_finder_register!(ctx, "I1", 126, 2, get_t5);
+    let m_pt = read_finder_register!(ctx, "Active power total", 140, 2, get_t6);
+    let m_qt = read_finder_register!(ctx, "Reactive power total", 148, 2, get_t6);
+    let m_st = read_finder_register!(ctx, "Apparent power total", 156, 2, get_t5);
+    let m_pft = read_finder_register!(ctx, "Power factor total", 164, 2, get_t7);
+    let m_temp = read_finder_register!(ctx, "Internal temperature", 181, 1, get_t17);
+    let m_u1_thd = read_finder_register!(ctx, "U1 THD%", 182, 1, get_t17);
+    let m_i1_thd = read_finder_register!(ctx, "I1 THD%", 188, 1, get_t17);
 
-    let raw_freq = ctx.read_input_registers(105, 2).await?;
-    let m_freq = get_t5(raw_freq.clone());
-    debug!("Frequency is '{:?}' => '{:?}'", raw_freq, m_freq);
+    // // C1 (MID certified) - Import Active Energy
+    let counter_c1 = read_finder_counter!(ctx, "C1", 401, 406, 462, 2638);
 
-    let raw_u1 = ctx.read_input_registers(107, 2).await?;
-    let m_u1 = get_t5(raw_u1.clone());
-    debug!("U1 is '{:?}' => '{:?}'", raw_u1, m_u1);
-
-    let raw_i1 = ctx.read_input_registers(126, 2).await?;
-    let m_i1 = get_t5(raw_i1.clone());
-    debug!("I1 is '{:?}' => '{:?}'", raw_i1, m_i1);
-
-    let raw_pt = ctx.read_input_registers(140, 2).await?;
-    let m_pt = get_t6(raw_pt.clone());
-    debug!("Active power total is '{:?}' => '{:?}'", raw_pt, m_pt);
-
-    let raw_qt = ctx.read_input_registers(148, 2).await?;
-    let m_qt = get_t6(raw_qt.clone());
-    debug!("Reactive power total is '{:?}' => '{:?}'", raw_qt, m_qt);
-
-    let raw_st = ctx.read_input_registers(156, 2).await?;
-    let m_st = get_t5(raw_st.clone());
-    debug!("Apparent power total is '{:?}' => '{:?}'", raw_st, m_st);
-
-    let raw_pft = ctx.read_input_registers(164, 2).await?;
-    let m_pft = get_t7(raw_pft.clone());
-    debug!("Power factor total is '{:?}' => '{:?}'", raw_pft, m_pft);
-
-    let raw_temp = ctx.read_input_registers(181, 1).await?;
-    let m_temp = get_t17(raw_temp.clone());
-    debug!("Internal temperature is '{:?}' => '{:?}'", raw_temp, m_temp);
-
-    let raw_u1_thd = ctx.read_input_registers(182, 1).await?;
-    let m_u1_thd = get_t17(raw_u1_thd.clone());
-    debug!("U1 THD% is '{:?}' => '{:?}'", raw_u1_thd, m_u1_thd);
-
-    let raw_i1_thd = ctx.read_input_registers(188, 1).await?;
-    let m_i1_thd = get_t17(raw_i1_thd.clone());
-    debug!("I1 THD% is '{:?}' => '{:?}'", raw_i1_thd, m_i1_thd);
-
-
-    //
-    // C1 (MID certified) - Import Active Energy
-    //
-    let raw_c1_exp = ctx.read_input_registers(401, 1).await?;
-    let m_c1_exp = get_t2(raw_c1_exp.clone()) as i32;
-    debug!("Energy counter c1 exponent is '{:?}' => '{:?}'", raw_c1_exp, m_c1_exp);
-
-    let raw_c1_mantissa = ctx.read_input_registers(406, 2).await?;
-    let m_c1_mantissa = get_t3(raw_c1_mantissa.clone());
-    debug!("Energy counter c1 mantissa is '{:?}' => '{:?}'", raw_c1_mantissa, m_c1_mantissa);
-
-    let m_c1_val = (m_c1_mantissa as f32) * (10.0_f32).powf(m_c1_exp as f32);
-    debug!("Energy counter c1 coarse value is '{:?}'", m_c1_val);
-
-    let raw_c1_x10 = ctx.read_input_registers(462, 2).await?;
-    let m_c1_x10 = get_t3(raw_c1_x10.clone()) as f32 / 10.0;
-    debug!("Energy counter c1 fine value is '{:?}' => '{:?}'", raw_c1_x10, m_c1_x10);
-
-    let raw_c1_float = ctx.read_input_registers(2638, 2).await?;
-    let m_c1_float = get_float(raw_c1_float.clone());
-    debug!("Energy counter c1 float value is '{:?}' => '{:?}'", m_c1_float, raw_c1_float);
-    
-    let counter_c1 = Counter {
-        exp: m_c1_exp,
-        mantissa: m_c1_mantissa,
-        val: m_c1_val,
-        x10: m_c1_x10,
-        float: m_c1_float
-    };
-
-    //
     // C4 (MID Certified) - Export reactive energy
-    //
-    let raw_c4_exp = ctx.read_input_registers(404, 1).await?;
-    let m_c4_exp = get_t2(raw_c4_exp.clone()) as i32;
-    debug!("Energy counter c4 exponent is '{:?}' => '{:?}'", raw_c4_exp, m_c4_exp);
+    let counter_c4 = read_finder_counter!(ctx, "C4", 404, 412, 468, 2644);
 
-    let raw_c4_mantissa = ctx.read_input_registers(412, 2).await?;
-    let m_c4_mantissa = get_t3(raw_c4_mantissa.clone());
-    debug!("Energy counter c4 mantissa is '{:?}' => '{:?}'", raw_c4_mantissa, m_c4_mantissa);
-
-    let m_c4_val = (m_c4_mantissa as f32) * (10.0_f32).powf(m_c4_exp as f32);
-    debug!("Energy counter c4 coarse value is '{:?}'", m_c4_val);
-
-    let raw_c4_x10 = ctx.read_input_registers(468, 2).await?;
-    let m_c4_x10 = get_t3(raw_c4_x10.clone()) as f32 / 10.0;
-    debug!("Energy counter c4 fine value is '{:?}' => '{:?}'", raw_c4_x10, m_c4_x10);
-
-    let raw_c4_float = ctx.read_input_registers(2644, 2).await?;
-    let m_c4_float = get_float(raw_c4_float.clone());
-    debug!("Energy counter c4 float value is '{:?}' => '{:?}'", m_c4_float, raw_c4_float);
-    
-    let counter_c4 = Counter {
-        exp: m_c4_exp,
-        mantissa: m_c4_mantissa,
-        val: m_c4_val,
-        x10: m_c4_x10,
-        float: m_c4_float
-    };
-
-    //
     // X3 (not certified) - Total Absolute Apparent Energy
-    //
-    let raw_x3_exp = ctx.read_input_registers(448, 1).await?;
-    let m_x3_exp = get_t2(raw_x3_exp.clone()) as i32;
-    debug!("Energy counter x3 exponent is '{:?}' => '{:?}'", raw_x3_exp, m_x3_exp);
+    let counter_x3 = read_finder_counter!(ctx, "X3", 448, 418, 474, 2764);
 
-    let raw_x3_mantissa = ctx.read_input_registers(418, 2).await?;
-    let m_x3_mantissa = get_t3(raw_x3_mantissa.clone());
-    debug!("Energy counter x3 mantissa is '{:?}' => '{:?}'", raw_x3_mantissa, m_x3_mantissa);
-
-    let m_x3_val = (m_x3_mantissa as f32) * (10.0_f32).powf(m_x3_exp as f32);
-    debug!("Energy counter x3 coarse value is '{:?}'", m_x3_val);
-
-    let raw_x3_x10 = ctx.read_input_registers(474, 2).await?;
-    let m_x3_x10 = get_t3(raw_x3_x10.clone()) as f32 / 10.0;
-    debug!("Energy counter x3 fine value is '{:?}' => '{:?}'", raw_x3_x10, m_x3_x10);
-
-    let raw_x3_float = ctx.read_input_registers(2764, 2).await?;
-    let m_x3_float = get_float(raw_x3_float.clone());
-    debug!("Energy counter x3 float value is '{:?}' => '{:?}'", m_x3_float, raw_x3_float);
-
-    let counter_x3 = Counter {
-        exp: m_x3_exp,
-        mantissa: m_x3_mantissa,
-        val: m_x3_val,
-        x10: m_x3_x10,
-        float: m_x3_float
-    };
-
-    let measurement = Measurement{
+    let measurement = Measurement {
         device_id: device_id as i32,
         device_timestamp: m_runtime,
         frequency: m_freq,
@@ -189,15 +78,18 @@ async fn get_measurements(modbus_conn_string: String, device_id: u8) -> Result<M
         temp: m_temp,
         c1: counter_c1,
         c4: counter_c4,
-        x3: counter_x3
+        x3: counter_x3,
     };
 
     Ok(measurement)
 }
 
 /// write results to psql
-async fn write_to_psql(psql_conn_string: String, measurement: Measurement) -> Result<(), Box<dyn std::error::Error>> {
-    use tokio_postgres::{NoTls};
+async fn write_to_psql(
+    psql_conn_string: String,
+    measurement: Measurement,
+) -> Result<(), Box<dyn std::error::Error>> {
+    use tokio_postgres::NoTls;
 
     let (client, connection) = tokio_postgres::connect(&psql_conn_string, NoTls).await?;
 
@@ -237,16 +129,20 @@ pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let log_level_str = settings.get_string("log_level").unwrap();
     let log_level = Level::from_str(&log_level_str).unwrap();
-    stderrlog::new().module(module_path!()).verbosity(log_level).init().unwrap();
+    stderrlog::new()
+        .module(module_path!())
+        .verbosity(log_level)
+        .init()
+        .unwrap();
 
     let modbus_addr: String = settings.get_string("modbus_server").unwrap();
     let modbus_device_id: u8 = settings.get_int("modbus_device_id").unwrap() as u8;
     let psql_addr: String = settings.get_string("psql").unwrap();
 
-    let measurement = get_measurements(modbus_addr, modbus_device_id).await.unwrap();
+    let measurement = get_measurements(modbus_addr, modbus_device_id)
+        .await
+        .unwrap();
     write_to_psql(psql_addr, measurement).await.unwrap();
 
     Ok(())
 }
-
-
